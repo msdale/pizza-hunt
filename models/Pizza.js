3ab7757b2de0e58e1cd22696a32cbf14fb1a5afb
Mark Dale <msdaledad@gmail.com>
2022-02-14 16:56:01 -0500

    update to 18.1.zip

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+
 const PizzaSchema = new Schema({
   pizzaName: {
     type: String
@@ -17,8 +18,6 @@ const PizzaSchema = new Schema({
   toppings: []
 });
 
-// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
