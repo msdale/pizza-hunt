ec09b00b07efe803bb3a2719047fc5b7f538ad36
Mark Dale <msdaledad@gmail.com>
2022-02-18 13:57:34 -0500

    completed

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt');
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
