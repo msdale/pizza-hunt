4642ac9e3145f2b0874ab900aa2dbe3d25b16f8a
Mark Dale <msdaledad@gmail.com>
2022-02-18 12:38:22 -0500

    completed 18.4

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -1,18 +1,21 @@
-// create variable to hold db connection
+// Global variables to manage IndexedDB connection
 let db;
+const idbPizzaHunt = 'pizza_hunt';
+const objStorePizza = 'new_pizza';
+
 // establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
-const request = window.indexedDB.open('pizza_hunt', 1);
+const request = window.indexedDB.open(idbPizzaHunt, 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
   // save a reference to the database
   const db = event.target.result;
   // create an object store (table) called 'new_pizza', set it to have an auto incrementing primary key of sorts
-  db.createObjectStore('new_pizza', { autoIncrement: true }); 
+  db.createObjectStore(objStorePizza, { autoIncrement: true });
 };
 
 // upon a successful request
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradeneeded event above) or...
   // simply established a connection, save reference to db in global variable
   db = event.target.result;
@@ -20,11 +23,69 @@ request.onsuccess = function(event) {
   // check if app is online, if yes run uploadPizza() function to send all local db data to api
   if (navigator.onLine) {
     // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadPizza();
+    uploadPizza();
   }
-}
+};
 
-request.onerror = function(event) {
+request.onerror = function (event) {
   // log error here
   console.log(event.target.errorCode);
-}
+};
+
+// this function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction([objStorePizza], 'readwrite');
+
+  // access the object store for `new_pizza`
+  const pizzaObjectStore = transaction.objectStore(objStorePizza);
+
+  // add record to your store with add method
+  pizzaObjectStore.add(record);
+};
+
+// this function will be executed when the app is brought back online
+function uploadPizza() {
+  // open a transaction on your db
+  const transaction = db.transaction([objStorePizza], 'readwrite');
+
+  // access your object store
+  const pizzaObjectStore = transaction.objectStore(objStorePizza);
+
+  // get all recores from store and set to a variable
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction([objStorePizza], 'readwrite');
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore(objStorePizza);
+          // clear all items in your store
+          pizzaObjectStore.clear();
+
+          alert('All saved pizza has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
+  // listen for app coming back online
+  window.addEventListener('online', uploadPizza);
+};
